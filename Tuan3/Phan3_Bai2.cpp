731670d599a8434e8fdb794c31859c1d61e815da
LeTanPhu <phule13112004@gmail.com>
2024-07-24 15:53:25 +0700

    Add Phan3_Bai2_Cau3,4

--- Tuan3/Phan3_Bai2.cpp
+++ Tuan3/Phan3_Bai2.cpp
@@ -29,6 +29,33 @@ void XuatPTCheoChinh(int a[][100], int n) {
 		}
 	}
 }
+void XuatPTSongSongCheoChinh(int a[][100], int n) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			if (i>j) printf("%d\t", a[i][j]);
+		}
+	}
+	printf("\n");
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			if (i < j) printf("%d\t", a[i][j]);
+		}
+	}
+}
+void TimMaxTamGiacTren(int a[][100], int n) {
+	int max = a[0][0];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			if (i < j ) {
+				if (a[i][j]>max) max = a[i][j];
+			}
+		}
+	}
+	printf("\nPhan tu max thuoc tam giac tren cua duong cheo chinh la %d", max);
+}
 int main() {
 	int a[100][100];
 	int n;
@@ -37,5 +64,9 @@ int main() {
 	XuatMTVuong(a, n);
 	printf("\n2.Xuat cac phan tu tren duong cheo chinh\n");
 	XuatPTCheoChinh(a, n);
+	printf("\n3.Xuat cac phan tu thuoc duong cheo song song voi duong cheo chinh\n");
+	XuatPTSongSongCheoChinh(a, n);
+	printf("\n4.Phan tu max thuoc tam giac tren cua duong cheo chinh");
+	TimMaxTamGiacTren(a, n);
 	getch();
 }
